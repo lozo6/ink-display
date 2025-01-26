6d40505744dc9ff1c1ec9a784b24ae34569fd26a
lozo6 <99729303+lozo6@users.noreply.github.com>
2025-01-26 18:33:17 -0500

    button test

--- display.py
+++ display.py
@@ -12,11 +12,10 @@ ecs = digitalio.DigitalInOut(board.CE0)
 dc = digitalio.DigitalInOut(board.D22)
 rst = digitalio.DigitalInOut(board.D27)
 busy = digitalio.DigitalInOut(board.D17)
+srcs = None
 
-display = Adafruit_SSD1680(
-    122, 250, spi, cs_pin=ecs, dc_pin=dc, sramcs_pin=None, rst_pin=rst, busy_pin=busy,
-)
-
+display = Adafruit_SSD1680(122, 250, spi, cs_pin=ecs, dc_pin=dc, sramcs_pin=srcs,
+                          rst_pin=rst, busy_pin=busy)
 display.fill(Adafruit_EPD.WHITE)
 
 display.fill_rect(0, 0, 50, 60, Adafruit_EPD.BLACK)
@@ -24,3 +23,14 @@ display.hline(80, 30, 60, Adafruit_EPD.BLACK)
 display.vline(80, 30, 60, Adafruit_EPD.BLACK)
 
 display.display()
+
+up_button = digitalio.DigitalInOut(board.D5)
+up_button.switch_to_input()
+down_button = digitalio.DigitalInOut(board.D6)
+down_button.switch_to_input()
+
+if not up_button.value:
+    print("Up Button Pushed")
+
+if not down_button.value:
+    print("Down Button Pushed")
