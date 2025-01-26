df9cc47a728f5f1bc21b9bf35044b47e6f4a76c3
lozo6 <99729303+lozo6@users.noreply.github.com>
2025-01-26 18:46:20 -0500

    test code updated

--- display.py
+++ display.py
@@ -18,18 +18,12 @@ print("Creating display")
 display = Adafruit_SSD1680(122, 250, spi, cs_pin=ecs, dc_pin=dc, sramcs_pin=srcs,
                           rst_pin=rst, busy_pin=busy)
 
+display.rotation = 1
+
 # clear the buffer
 print("Clear buffer")
 display.fill(Adafruit_EPD.WHITE)
 display.pixel(10, 100, Adafruit_EPD.BLACK)
-
-print("Draw Rectangles")
-display.fill_rect(5, 5, 10, 10, Adafruit_EPD.RED)
-display.rect(0, 0, 20, 30, Adafruit_EPD.BLACK)
-
-print("Draw lines")
-display.line(0, 0, display.width-1, display.height-1, Adafruit_EPD.BLACK)
-display.line(0, display.height-1, display.width-1, 0, Adafruit_EPD.RED)
 
 print("Draw text")
 display.text('hello world', 25, 10, Adafruit_EPD.BLACK)
