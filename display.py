0ff72176affa4707094360476cec5b8ee3933230
lozo6 <99729303+lozo6@users.noreply.github.com>
2025-01-26 18:58:45 -0500

    reverted code

--- display.py
+++ display.py
@@ -23,18 +23,6 @@ from adafruit_epd.uc8151d import Adafruit_UC8151D  # pylint: disable=unused-impo
 from adafruit_epd.ek79686 import Adafruit_EK79686  # pylint: disable=unused-import
 from adafruit_epd.ssd1680 import Adafruit_SSD1680Z
 
-# First define some color constants
-WHITE = (0xFF, 0xFF, 0xFF)
-BLACK = (0x00, 0x00, 0x00)
-RED = (0xFF, 0x00, 0x00)
-
-# Next define some constants to allow easy resizing of shapes and colors
-BORDER = 20
-FONTSIZE = 24
-BACKGROUND_COLOR = BLACK
-FOREGROUND_COLOR = WHITE
-TEXT_COLOR = RED
-
 # create the spi device and pins we will need
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 ecs = digitalio.DigitalInOut(board.CE0)
@@ -76,59 +64,26 @@ display.rotation = 1
 
 # ---
 
-# image = Image.open("blinka.png")
+image = Image.open("blinka.png")
 
-# # Scale the image to the smaller screen dimension
-# image_ratio = image.width / image.height
-# screen_ratio = display.width / display.height
-# if screen_ratio < image_ratio:
-#     scaled_width = image.width * display.height // image.height
-#     scaled_height = display.height
-# else:
-#     scaled_width = display.width
-#     scaled_height = image.height * display.width // image.width
-# image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+# Scale the image to the smaller screen dimension
+image_ratio = image.width / image.height
+screen_ratio = display.width / display.height
+if screen_ratio < image_ratio:
+    scaled_width = image.width * display.height // image.height
+    scaled_height = display.height
+else:
+    scaled_width = display.width
+    scaled_height = image.height * display.width // image.width
+image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
 
-# # Crop and center the image
-# x = scaled_width // 2 - display.width // 2
-# y = scaled_height // 2 - display.height // 2
-# image = image.crop((x, y, x + display.width, y + display.height)).convert("RGB")
+# Crop and center the image
+x = scaled_width // 2 - display.width // 2
+y = scaled_height // 2 - display.height // 2
+image = image.crop((x, y, x + display.width, y + display.height)).convert("RGB")
 
-# # Convert to Monochrome and Add dithering
-# image = image.convert("1").convert("L")
-
-# # Display image.
-# display.image(image)
-# display.display()
-
-# ---
-
-image = Image.new("RGB", (display.width, display.height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a filled box as the background
-draw.rectangle((0, 0, display.width - 1, display.height - 1), fill=BACKGROUND_COLOR)
-
-# Draw a smaller inner foreground rectangle
-draw.rectangle(
-    (BORDER, BORDER, display.width - BORDER - 1, display.height - BORDER - 1),
-    fill=FOREGROUND_COLOR,
-)
-
-# Load a TTF Font
-font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", FONTSIZE)
-
-# Draw Some Text
-text = "Hello World!"
-(font_width, font_height) = font.getsize(text)
-draw.text(
-    (display.width // 2 - font_width // 2, display.height // 2 - font_height // 2),
-    text,
-    font=font,
-    fill=TEXT_COLOR,
-)
+# Convert to Monochrome and Add dithering
+image = image.convert("1").convert("L")
 
 # Display image.
 display.image(image)
