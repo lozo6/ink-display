c59e5bf77200ba68fedccbb603f6f5fa82fa0314
lozo6 <99729303+lozo6@users.noreply.github.com>
2025-01-28 02:24:02 -0500

    trying different class

--- button.py
+++ button.py
@@ -2,7 +2,7 @@ import digitalio
 import busio
 import board
 from adafruit_epd.epd import Adafruit_EPD
-from adafruit_epd.ssd1680 import Adafruit_SSD1680Z
+from adafruit_epd.ssd1680 import Adafruit_SSD1680
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 ecs = digitalio.DigitalInOut(board.CE0)
@@ -11,7 +11,7 @@ rst = digitalio.DigitalInOut(board.D27)
 busy = digitalio.DigitalInOut(board.D17)
 srcs = None
 
-display = Adafruit_SSD1680Z(122, 250,        # 2.13" HD Tri-color or mono display
+display = Adafruit_SSD1680(122, 250,        # 2.13" HD Tri-color or mono display
     spi,
     cs_pin=ecs,
     dc_pin=dc,
