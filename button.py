0ba672b4c87412b5710ca675c3b920ed7f78d3d4
lozo6 <99729303+lozo6@users.noreply.github.com>
2025-01-28 02:27:40 -0500

    added different spi device and pins

--- button.py
+++ button.py
@@ -4,12 +4,13 @@ import board
 from adafruit_epd.epd import Adafruit_EPD
 from adafruit_epd.ssd1680 import Adafruit_SSD1680
 
+# create the spi device and pins we will need
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-ecs = digitalio.DigitalInOut(board.CE0)
-dc = digitalio.DigitalInOut(board.D22)
-rst = digitalio.DigitalInOut(board.D27)
-busy = digitalio.DigitalInOut(board.D17)
-srcs = None
+ecs = digitalio.DigitalInOut(board.D12)
+dc = digitalio.DigitalInOut(board.D11)
+srcs = digitalio.DigitalInOut(board.D10)  # can be None to use internal memory
+rst = digitalio.DigitalInOut(board.D9)  # can be None to not use this pin
+busy = digitalio.DigitalInOut(board.D5)  # can be None to not use this pin
 
 display = Adafruit_SSD1680(122, 250,        # 2.13" HD Tri-color or mono display
     spi,
