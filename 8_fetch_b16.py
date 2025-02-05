9510a79a67b42e8426cad9031d4614a2c402798a
lozo6 <99729303+lozo6@users.noreply.github.com>
2025-02-05 02:33:27 -0500

    fixed typo

--- 8_fetch_b16.py
+++ 8_fetch_b16.py
@@ -1,18 +1,19 @@
 import time
+import os
 from dotenv import load_dotenv
 from display_manager import DisplayManager
 from bus_graphics import BusGraphics
 
 # Load environment variables
 load_dotenv()
-ALPHA_VANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+MTA_BUS_TOKEN = os.getenv("MTA_BUS_TOKEN")
 
 # Initialize the e-ink display
 display_manager = DisplayManager()
 display = display_manager.get_display()
 
-# Create BusGraphics instance
-bus_graphics = BusGraphics(display)
+# Create BusGraphics instance with API key
+bus_graphics = BusGraphics(display, MTA_BUS_TOKEN)
 
 while True:
     bus_graphics.fetch_bus_data()
