4bd305c350143ff0606ea4f5c82c65a6d9854232
lozo6 <99729303+lozo6@users.noreply.github.com>
2025-02-08 14:53:23 -0500

    changed size of qrcode

--- wifi_graphics.py
+++ wifi_graphics.py
@@ -19,7 +19,7 @@ class Wifi_Graphics:
         wifi_qr_data = f"WIFI:S:{ssid};T:{auth};P:{password};;"
         qr = qrcode.make(wifi_qr_data)
         # Resize for e-ink
-        qr = qr.resize((135, 135), Image.Resampling.LANCZOS)
+        qr = qr.resize((140, 140), Image.Resampling.LANCZOS)
         return qr
 
     def display_qr(self, ssid, password, auth):
@@ -29,7 +29,7 @@ class Wifi_Graphics:
         self.draw.text((10, 30), "Connect to", font=font, fill=0)
         self.draw.text((15, 50), "Internet:", font=font, fill=0)
 
-        self.image.paste(qr, (125, -10))
+        self.image.paste(qr, (120, -10))
 
         # Update e-ink display
         self.display.image(self.image)
