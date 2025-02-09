f664a640b4614592632ebf7ded62abf5dfa52851
lozo6 <99729303+lozo6@users.noreply.github.com>
2025-02-08 21:43:15 -0500

    another button test

--- 3_button.py
+++ 3_button.py
@@ -1,15 +1,15 @@
-import digitalio
 import busio
 import board
+from digitalio import DigitalInOut, Direction
 from adafruit_epd.epd import Adafruit_EPD
 from adafruit_epd.ssd1680 import Adafruit_SSD1680Z
 
 # create the spi device and pins we will need
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-ecs = digitalio.DigitalInOut(board.CE0)
-dc = digitalio.DigitalInOut(board.D22)
-rst = digitalio.DigitalInOut(board.D27)
-busy = digitalio.DigitalInOut(board.D17)
+ecs = DigitalInOut(board.CE0)
+dc = DigitalInOut(board.D22)
+rst = DigitalInOut(board.D27)
+busy = DigitalInOut(board.D17)
 srcs = None
 
 display = Adafruit_SSD1680Z(
@@ -23,10 +23,10 @@ display = Adafruit_SSD1680Z(
     busy_pin=busy,
 )
 
-up_button = digitalio.DigitalInOut(board.D5)
-up_button.switch_to_input()
-down_button = digitalio.DigitalInOut(board.D6)
-down_button.switch_to_input()
+up_button = DigitalInOut(board.D5)
+up_button.direction = Direction.INPUT
+down_button = DigitalInOut(board.D6)
+down_button.direction = Direction.INPUT
 
 while True:
     if not up_button.value:
